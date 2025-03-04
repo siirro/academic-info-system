502d1575b5db51d4b127a9d1c41a194d6ab61bc0
siirro <viostarryy@gmail.com>
2025-03-04 19:56:57 +0900

    [미션1] 깃허브 리포지토리에 프로젝트 올리기

---
+++ src/main/kotlin/com/report/academic_info_system/AcademicInfoSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.report.academic_info_system
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AcademicInfoSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<AcademicInfoSystemApplication>(*args)
+}
---
+++ src/test/kotlin/com/report/academic_info_system/AcademicInfoSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.report.academic_info_system
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AcademicInfoSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
