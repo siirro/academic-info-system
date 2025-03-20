189a0ab383269ef8ef6161081fce8891b81bc909
siirro <viostarryy@gmail.com>
2025-03-20 21:29:28 +0900

    feat: Exception, Advise 생성

--- src/main/kotlin/com/report/academic_info_system/admin/exception/AdminException.kt
+++ src/main/kotlin/com/report/academic_info_system/admin/exception/AdminException.kt
@@ -8,3 +8,7 @@ abstract class AdminException(
 ) : RuntimeException(message) {
     val httpStatus: HttpStatus = httpStatus
 }
+
+class AdminBadRequestException(message: String) : AdminException(HttpStatus.BAD_REQUEST, message)
+
+class AdminInternalServerErrorException(message: String) : AdminException(HttpStatus.INTERNAL_SERVER_ERROR, message)
